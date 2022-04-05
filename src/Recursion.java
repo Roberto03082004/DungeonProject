cc8d2ebe3fb65f7ab58ca4693628a8c32289141d
felos <fsolorzano23378@columbushs.com>
2022-04-05 09:21:00 -0400

    Recursion added

---
+++ src/Recursion.java
@@ -0,0 +1,11 @@
+
+public class Recursion {
+
+    static String recursion(int n ) {
+        while (n > 0) {
+            System.out.println("WELCOME!");
+            n--;
+        }
+        return "WElCOME TO DUNGEON!";
+    }
+}
--- src/TextAdventure.java
+++ src/TextAdventure.java
@@ -1,6 +1,9 @@
 public class TextAdventure {
 
     public static void main(String[] args) {
+
+        System.out.println(Recursion.recursion(4));
+
         AdventureModel adv = new AdventureModel();
         adv.startGame();
     }
